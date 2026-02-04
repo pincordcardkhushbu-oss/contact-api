064493d9a293e896d439018830182fc1f55d7810
pincordcardkhushbu-oss <OWNER_GITHUB_EMAIL@gmail.com>
2026-02-04 17:18:37 +0530

    mod

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,60 +1,151 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const router = express.Router()
+const User = require('../model/user')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const User = require('../../model/user')
+//signup api
+router.post('/signup',(req,res)=>{
+    console.log('signup post request')
 
-/**
- * SIGNUP USER
- * POST /api/user/signup
- */
-router.post('/signup', async (req, res) => {
-  try {
-    const { firstName, lastName, email, password } = req.body
+    User.find({email:req.body.email})
+    .then(result=>{
+        if(result.length>0)
+        {
+            return res.status(500).json({
+                msg:'email already exist'
+            })
+        }
 
-    // validation
-    if (!firstName || !lastName || !email || !password) {
-      return res.status(400).json({
-        msg: 'All fields are required'
-      })
-    }
+        bcrypt.hash(req.body.password,10,(err,hash)=>{
+        if(err)
+        {
+            console.log(err)
+            return res.status(500).json({
+                error:err
+            })
+        }
 
-    // check if email already exists
-    const existingUser = await User.findOne({ email })
-    if (existingUser) {
-      return res.status(409).json({
-        msg: 'Email already exists'
-      })
-    }
-
-    // create user
-    const user = new User({
-      _id: new mongoose.Types.ObjectId(),
-      firstName,
-      lastName,
-      email,
-      password
+        const newUser = new User({
+        _id:new mongoose.Types.ObjectId,
+        firstName:req.body.firstName,
+        lastName:req.body.lastName,
+        email:req.body.email,
+        password:hash
+    })
+    newUser.save()
+    .then(result=>{
+        const token = jwt.sign({
+            firstName:result.firstName,
+            lastName:result.lastName,
+            email:result.email,
+            userId:result._id
+        },
+            'sbs 147',
+            {
+                expiresIn:"365d"
+            }
+        )
+        res.status(200).json({
+            firstName:result.firstName,
+            lastName:result.lastName,
+            email:result.email,
+            userId:result._id,
+            token:token
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
     })
 
-    await user.save()
-
-    res.status(201).json({
-      msg: 'User created successfully',
-      user: {
-        id: user._id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email
-      }
     })
 
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({
-      error: error.message
     })
-  }
+    .catch(err=>{
+        res.status(500).json({
+            error:err
+        })
+    })
+
+    
 })
 
-module.exports = router
- 
+
+// login api
+
+router.post('/login',(req,res)=>{
+    console.log(req.body)
+    User.find({email:req.body.email})
+    .then(user=>{
+        console.log(user)
+        if(user.length<1)
+        {
+            return res.status(401).json({
+                msg:'user not exist'
+            })
+        }
+
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(!result)
+            {
+                return res.status(401).json({
+                    msg:'invalid password'
+                })
+            }
+
+            //creating token
+            const token = jwt.sign({
+                firstName:user[0].firstName,
+                lastName:user[0].lastName,
+                email:user[0].email,
+                userId:user[0]._id
+            },
+                'sbs 147',
+                {
+                    expiresIn:"365d"
+                }
+            )
+
+            res.status(200).json({
+                firstName:user[0].firstName,
+                lastName:user[0].lastName,
+                email:user[0].email,
+                userId:user[0]._id,
+                token:token
+            })
+        })
+
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
+//check user
+router.get('/checkEmail/:email',(req,res)=>{
+    User.find({email:req.params.email})
+    .then(result=>{
+        if(result.length > 0)
+        {
+            return res.status(200).json({
+                isAvailable:true
+            })
+        }
+        res.status(200).json({
+            isAvailable:false
+        })
+
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
+})
+
+module.exports = router;
